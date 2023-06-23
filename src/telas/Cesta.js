6370ec97115cd86acf6342953676d97d5a24c635
Vinicius Diefenbach <viniciusdiefenbach@adm.ifsul.edu.br>
2023-06-23 13:40:22 -0300

    criando descricao da cesta

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,6 +1,7 @@
-import { Dimensions, Image, StyleSheet, Text } from "react-native";
+import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
 
-import topo from "../../assets/topo.png"
+import topo from "../../assets/topo.png";
+import logo from "../../assets/logo.png";
 
 const width = Dimensions.get("screen").width;
 
@@ -9,6 +10,21 @@ export default function Cesta() {
         <>
             <Image style={estilos.topo} source={topo} />
             <Text style={estilos.titulo}>Detalhes da cesta</Text>
+            <View style={estilos.cesta}>
+                <Text style={estilos.nome}>Cesta de Verduras</Text>
+                <View style={estilos.fazenda}>
+                    <Image style={estilos.imagemFazenda} source={logo} />
+                    <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+                </View>
+                <Text style={estilos.descricao}>
+                    Uma cesta com produtos selecionados
+                    cuidadosamente da fazendo para
+                    sua cozinha
+                </Text>
+                <Text style={estilos.preco}>
+                    R$ 40,00
+                </Text>
+            </View>
         </>
     );
 }
@@ -27,5 +43,40 @@ const estilos = StyleSheet.create({
         color: "white",
         fontWeight: "bold",
         padding: 16
+    },
+    cesta: {
+        paddingVertical: 20,
+        paddingHorizontal: 16
+    },
+    nome: {
+        color: "#464646",
+        fontSize: 26,
+        lineHeight: 42,
+        fontWeight: "bold"
+    },
+    fazenda: {
+        flexDirection: "row",
+        paddingVertical: 12,
+        gap: 12
+    },
+    imagemFazenda: {
+        height: 32,
+        width: 32
+    },
+    nomeFazenda: {
+        fontSize: 20,
+        lineHeight: 26
+    },
+    descricao: {
+        color: "#a3a3a3",
+        fontSize: 16,
+        lineHeight: 26
+    },
+    preco: {
+        color: "#2a9f85",
+        fontWeight: "bold",
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 8
     }
 });
